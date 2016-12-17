464a513a2b5f67f371d06878526f4fd946e5c5a2
Yorick <livchak11@gmail.com>
2016-12-17 17:43:15 +0200

    Clean template

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'lits-project'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/lits-project-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'lits-project'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/lits-project-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'lits-project'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/lits-project-production'
+  }
+};
+
+module.exports = config[env];
